a089265696184aed46eeda3a4450c264b3b179e6
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2014-09-26 20:09:37 +0200

    Refactored Promise implementaion with more features and robustness

--- src/api/promise.js
+++ src/api/promise.js
@@ -1,13 +1,27 @@
-
-// A generic promiese interface by using riot.observable
-
-function Promise(fn) {
-  var self = riot.observable(this);
-
-  $.map(['done', 'fail', 'always'], function(name) {
-    self[name] = function(arg) {
-      return self[$.isFunction(arg) ? 'on' : 'trigger'](name, arg);
-    };
-  });
-
+// Simple Promise pattern using riot.observable
+function Promise() {
+    var self = riot.observable(this);
+    self.resolution = null;
+    self.result = null;
+    $.map(['done', 'fail', 'always'], function(name) {
+        self[name] = function(arg) {
+            if($.isFunction(arg)) {
+                self.one(name, arg);
+                if(self.resolution === name || (self.resolution !== null && name === 'always')) {
+                    self.trigger(name, self.result);
+                }
+            } else {
+                if(name === 'always') {
+                    throw new Error('Invalid argument for "always" function: ' + arg);
+                }
+                if(self.resolution !== null) {
+                    throw new Error('Can not resolve promise - already resolved to ' + self.resolution)
+                }
+                self.resolution = name;
+                self.result = arg;
+                self.trigger(name, self.result).trigger('always', self.result);
+            }
+            return self;
+        };
+    });
 }
